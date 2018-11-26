042aa7351a431f19e25033d7163e00d4fba5f804
Light-Unlighted <44262036+Light-Unlighted@users.noreply.github.com>
2018-11-26 10:01:35 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+var tablePat = ["tapote la tête de", 
+				"frotte la tête de",
+ 				"caresse la tête de"]
+
+client.on('message', message => {
+	let cmember = message.mentions.members.first();
+    if (message.content.startsWith(".pat ")) {
+       var Random = Math.floor(Math.random() * 2)
+        message.channel.send( message.author + tablePat[Random] + cmember.toString())
+    }
+
+})
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NTE2NTM4MjkwODcxMDc0ODI3.Dt1Hkg.lGHflFlz8h71yOHrJG_0G1LIffE);
