864a4ac605cb8ead14019bfe4abf6cda4a2b14bb
Light-Unlighted <44262036+Light-Unlighted@users.noreply.github.com>
2018-11-26 10:02:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bot Starter');
 });
 
 client.on('message', message => {
